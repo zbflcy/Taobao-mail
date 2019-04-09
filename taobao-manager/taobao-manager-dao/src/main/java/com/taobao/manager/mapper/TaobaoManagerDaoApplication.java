3a9098b68215b25810397912799aa182223dbf51
zbflcy <mingtian4811640@163.com>
2019-04-09 15:01:17 +0800

    init

---
+++ taobao-manager/taobao-manager-dao/src/main/java/com/taobao/manager/mapper/TaobaoManagerDaoApplication.java
@@ -0,0 +1,13 @@
+package com.taobao.manager.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaobaoManagerDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaobaoManagerDaoApplication.class, args);
+    }
+
+}
